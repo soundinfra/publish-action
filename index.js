0e7f308d7d3d20bfc95b59257ce9321504ab4f2a
David Griffin <david@soundinfra.com>
2021-06-01 16:18:00 -0700

    Implement very basic test of HTTP get function.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const https = require('https')
+
+// https://blog.logrocket.com/5-ways-to-make-http-requests-in-node-js/
+exports.get = function http_get(url) {
+  url = 'https://jsonplaceholder.typicode.com/users'
+  https.get(url, res => {
+    let data = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no Date header';
+    console.log('Status Code:', res.statusCode);
+    console.log('Date in Response header: ', headerDate);
+    res.on('data', chunk => {
+      data.push(chunk)
+    });
+    res.on('end', () => {
+      console.log('Response ended')
+      const users = JSON.parse(Buffer.concat(data).toString());
+      for (user of users) {
+        console.log(`Got user with id: ${user.id}, name: ${user.name}`)
+      };
+    }).on('error', err => {
+      console.log('Error: ', err.message)
+    })
+  });
+  return url
+};
 
 try {
-  // `who-to-greet` input defined in action metadata file
+  // Inputs defined in action metadata file.
   const publishDomain = core.getInput('publish-domain');
   const publishDirectory = core.getInput('publish-directory');
   console.log(`Publishing from ${publishDirectory} to ${publishDomain}!`);
+  http_get("dummy");
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
--- test/test.js
+++ test/test.js
@@ -1,4 +1,12 @@
 var assert =  require('assert');
+const {get} = require("../index.js");
+
+describe("Get", function() {
+    it("do a get", function () {
+      url = 'https://jsonplaceholder.typicode.com/users'
+      assert.equal(get("hello"), url)
+    });
+});
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when value is not present', function() {
